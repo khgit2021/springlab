ca2a5bcfb811eebe688d6bd2d7dba149d64f19c9
Khoa Ho <khho_jn9@yahoo.com>
2021-05-19 17:45:15 -0400

    Update data.sql and add readme file

--- springlab/src/main/resources/data.sql
+++ springlab/src/main/resources/data.sql
@@ -1,1 +1,2 @@
 insert into training(id, emp_id, descr, training_date) values(9101, 2101, 'Class 2101', CURDATE());
+insert into training(id, emp_id, descr, training_date) values(9102, 2102, 'Class 2102', CURDATE());
